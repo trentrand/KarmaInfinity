5ab2c275f982552582cb217c696d672fdc5aed65
Trent Rand <Trentmrand@gmail.com>
2014-03-24 11:20:03 -0700

    Surrounded Main Screen in a JPanel

--- src/com/appliesinc/UpVoteBot/View.java
+++ src/com/appliesinc/UpVoteBot/View.java
@@ -32,7 +32,6 @@ import javax.swing.event.ChangeListener;
 
 import org.json.simple.parser.ParseException;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class View.
  */
@@ -43,7 +42,7 @@ public class View {
 
 	/** The Logic object to use. */
 	public static Logic logic;
-	
+
 	/**
 	 * The main method.
 	 * 
@@ -128,6 +127,7 @@ public class View {
 
 	/** The txt username. */
 	private JFormattedTextField txtUsername;
+	private JScrollPane scrollPane;
 
 	/**
 	 * Instantiates a new view.
@@ -143,86 +143,24 @@ public class View {
 
 		// Initialize and setup frameApplication JFrame
 		frameApplication = new JFrame();
-		frameApplication.setTitle("Karma \u221E by applies inc.\n");
+		frameApplication.setTitle("Karma \u221E by trent rand");
 		frameApplication.setFont(new Font("Courier New", Font.PLAIN, 12));
-		frameApplication.setBounds(100, 100, 451, 604);
+		frameApplication.setBounds(100, 100, 488, 604);
 		frameApplication.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frameApplication.getContentPane().setLayout(null);
 
-		// Initialize and setup separator JSeperator
-		JSeparator separator = new JSeparator();
-		separator.setBounds(6, 165, 438, 12);
-		frameApplication.getContentPane().add(separator);
-
-		// Initialize and setup Post-Address JLabel
-		JLabel lblPageName = new JLabel("Page Name:");
-		lblPageName.setBounds(77, 551, 109, 16);
-		lblPageName.setFont(new Font("Courier New", Font.PLAIN, 16));
-		frameApplication.getContentPane().add(lblPageName);
-
-		// Initialize and setup Get-Karma JButton
-		btnGetKarma = new JButton("Get Karma");
-		btnGetKarma.setBounds(327, 547, 117, 29);
-		btnGetKarma.setFont(new Font("Courier New", Font.PLAIN, 13));
-		btnGetKarma.setEnabled(false);
-		frameApplication.getContentPane().add(btnGetKarma);
-
-		txtSubReddit = new JTextField();
-		txtSubReddit.setBounds(183, 545, 143, 28);
-		txtSubReddit.setFont(new Font("Courier New", Font.PLAIN, 13));
-		frameApplication.getContentPane().add(txtSubReddit);
-		txtSubReddit.setColumns(10);
-
 		submissionList = new DefaultListModel<String>();
-
-		btnPagePrevious = new JButton("Previous");
-		btnPagePrevious.setBounds(38, 182, 105, 29);
-		btnPagePrevious.setEnabled(false);
-		btnPagePrevious.setFont(new Font("Courier New", Font.PLAIN, 13));
-		frameApplication.getContentPane().add(btnPagePrevious);
-
-		btnPageNext = new JButton("Next");
-		btnPageNext.setBounds(305, 182, 105, 29);
-		btnPageNext.setEnabled(false);
-		btnPageNext.setFont(new Font("Courier New", Font.PLAIN, 13));
-		frameApplication.getContentPane().add(btnPageNext);
-
-		lblPageNumber = new JLabel("Page: " + page);
-		lblPageNumber.setBounds(140, 187, 168, 16);
-		lblPageNumber.setEnabled(false);
-		lblPageNumber.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPageNumber.setFont(new Font("Courier New", Font.PLAIN, 15));
-		frameApplication.getContentPane().add(lblPageNumber);
-
-		btnFetchPosts = new JButton("Fetch Posts");
-		btnFetchPosts.setBounds(315, 501, 117, 29);
-		btnFetchPosts.setEnabled(false);
-		btnFetchPosts.setFont(new Font("Courier New", Font.PLAIN, 13));
-		frameApplication.getContentPane().add(btnFetchPosts);
-
-		lblFetchedPage = new JLabel();
-		lblFetchedPage.setBounds(28, 501, 285, 18);
-		lblFetchedPage.setFont(new Font("Courier New", Font.PLAIN, 13));
-		lblFetchedPage.setText(this.getTxtSubReddit().getText());
-		frameApplication.getContentPane().add(lblFetchedPage);
 
 		btnGroupPageType = new ButtonGroup();
 
-		rdBtnTypeSubreddit = new JRadioButton("/r/");
-		rdBtnTypeSubreddit.setBounds(6, 551, 141, 23);
-
-		rdBtnTypeUser = new JRadioButton("/u/");
-		rdBtnTypeUser.setBounds(6, 531, 141, 23);
-
-		btnGroupPageType.add(rdBtnTypeUser);
-		btnGroupPageType.add(rdBtnTypeSubreddit);
-
-		frameApplication.getContentPane().add(rdBtnTypeSubreddit);
-		frameApplication.getContentPane().add(rdBtnTypeUser);
+		JPanel panel = new JPanel();
+		panel.setBounds(6, 6, 498, 576);
+		frameApplication.getContentPane().add(panel);
+		panel.setLayout(null);
 
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(28, 6, 404, 171);
-		frameApplication.getContentPane().add(tabbedPane);
+		tabbedPane.setBounds(47, 6, 404, 171);
+		panel.add(tabbedPane);
 
 		JPanel singlePanel = new JPanel();
 		tabbedPane.addTab("Single User", null, singlePanel, null);
@@ -270,38 +208,91 @@ public class View {
 		JLabel lblTodo = new JLabel("TODO");
 		multiPanel.add(lblTodo);
 
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(38, 211, 382, 278);
-		frameApplication.getContentPane().add(scrollPane);
+		// Initialize and setup separator JSeperator
+		JSeparator separator = new JSeparator();
+		separator.setBounds(35, 170, 428, 12);
+		panel.add(separator);
+
+		lblPageNumber = new JLabel("Page: " + page);
+		lblPageNumber.setBounds(190, 194, 117, 16);
+		panel.add(lblPageNumber);
+		lblPageNumber.setEnabled(false);
+		lblPageNumber.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPageNumber.setFont(new Font("Courier New", Font.PLAIN, 15));
+
+		btnPagePrevious = new JButton("Previous");
+		btnPagePrevious.setBounds(35, 189, 105, 29);
+		panel.add(btnPagePrevious);
+		btnPagePrevious.setEnabled(false);
+		btnPagePrevious.setFont(new Font("Courier New", Font.PLAIN, 13));
+
+		btnPageNext = new JButton("Next");
+		btnPageNext.setBounds(346, 189, 105, 29);
+		panel.add(btnPageNext);
+		btnPageNext.setEnabled(false);
+		btnPageNext.setFont(new Font("Courier New", Font.PLAIN, 13));
+
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(32, 222, 419, 274);
+		panel.add(scrollPane);
 
 		listSubmissions = new JList<String>(submissionList);
 		scrollPane.setViewportView(listSubmissions);
 		listSubmissions.setBorder(BorderFactory
 				.createTitledBorder("Submissions"));
 
+		btnFetchPosts = new JButton("Fetch Posts");
+		btnFetchPosts.setBounds(346, 514, 117, 29);
+		panel.add(btnFetchPosts);
+		btnFetchPosts.setEnabled(false);
+		btnFetchPosts.setFont(new Font("Courier New", Font.PLAIN, 13));
 
-		
-		/** Action Listeners. */
-		btnSignIn.addActionListener(new ActionListener() {
+		// Initialize and setup Get-Karma JButton
+		btnGetKarma = new JButton("Get Karma");
+		btnGetKarma.setBounds(346, 539, 117, 29);
+		panel.add(btnGetKarma);
+		btnGetKarma.setFont(new Font("Courier New", Font.PLAIN, 13));
+		btnGetKarma.setEnabled(false);
 
+		txtSubReddit = new JTextField();
+		txtSubReddit.setBounds(153, 539, 191, 28);
+		panel.add(txtSubReddit);
+		txtSubReddit.setFont(new Font("Courier New", Font.PLAIN, 13));
+		txtSubReddit.setColumns(10);
+
+		// Initialize and setup Post-Address JLabel
+		JLabel lblPageName = new JLabel("Page Name:");
+		lblPageName.setBounds(35, 543, 109, 16);
+		panel.add(lblPageName);
+		lblPageName.setFont(new Font("Courier New", Font.PLAIN, 16));
+
+		rdBtnTypeSubreddit = new JRadioButton("/r/");
+		rdBtnTypeSubreddit.setBounds(35, 520, 57, 23);
+		panel.add(rdBtnTypeSubreddit);
+		btnGroupPageType.add(rdBtnTypeSubreddit);
+
+		rdBtnTypeUser = new JRadioButton("/u/");
+		rdBtnTypeUser.setBounds(35, 500, 57, 23);
+		panel.add(rdBtnTypeUser);
+
+		btnGroupPageType.add(rdBtnTypeUser);
+
+		lblFetchedPage = new JLabel();
+		lblFetchedPage.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblFetchedPage.setBounds(243, 500, 207, 18);
+		panel.add(lblFetchedPage);
+		lblFetchedPage.setFont(new Font("Courier New", Font.PLAIN, 13));
+		lblFetchedPage.setText(this.getTxtSubReddit().getText());
+		rdBtnTypeUser.addChangeListener(new ChangeListener() {
 			@Override
-			public void actionPerformed(ActionEvent evt) {
-				logic.btnSignInPressed();
+			public void stateChanged(ChangeEvent e) {
+				logic.rdBtnTypeUserStateChanged();
 			}
 		});
-
-		btnGetKarma.addActionListener(new ActionListener() {
+		rdBtnTypeSubreddit.addChangeListener(new ChangeListener() {
 			@Override
-			public void actionPerformed(ActionEvent evt) {
-				try {
-					logic.btnGetKarmaPressed();
-				} catch (IOException e) {
-					System.out.println("Exception: " + e.getMessage() + "in btnGetKarmaPressed()");
-					e.printStackTrace();
-				} catch (ParseException e) {
-					System.out.println("Exception: " + e.getMessage() + "in btnGetKarmaPressed()");
-					e.printStackTrace();
-				}
+			public void stateChanged(ChangeEvent e) {
+				logic.rdBtnTypeSubredditStateChanged();
 			}
 		});
 
@@ -313,10 +304,37 @@ public class View {
 
 		});
 
-		btnPagePrevious.addActionListener(new ActionListener() {
+		btnGetKarma.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent evt) {
-				logic.btnPagePreviousPressed();
+				try {
+					logic.btnGetKarmaPressed();
+				} catch (IOException e) {
+					System.out.println("Exception: " + e.getMessage()
+							+ "in btnGetKarmaPressed()");
+					e.printStackTrace();
+				} catch (ParseException e) {
+					System.out.println("Exception: " + e.getMessage()
+							+ "in btnGetKarmaPressed()");
+					e.printStackTrace();
+				}
+			}
+		});
+
+		btnFetchPosts.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				try {
+					logic.btnFetchPosts();
+				} catch (IOException e) {
+					System.out.println("Exception: " + e.getMessage()
+							+ "in btnFetchPosts()");
+					e.printStackTrace();
+				} catch (ParseException e) {
+					System.out.println("Exception: " + e.getMessage()
+							+ "in btnFetchPosts()");
+					e.printStackTrace();
+				}
 			}
 		});
 
@@ -327,30 +345,19 @@ public class View {
 			}
 		});
 
-		btnFetchPosts.addActionListener(new ActionListener() {
+		btnPagePrevious.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent evt) {
-					try {
-						logic.btnFetchPosts();
-					} catch (IOException e) {
-						System.out.println("Exception: " + e.getMessage() + "in btnFetchPosts()");
-						e.printStackTrace();
-					} catch (ParseException e) {
-						System.out.println("Exception: " + e.getMessage() + "in btnFetchPosts()");
-						e.printStackTrace();
-					}
+				logic.btnPagePreviousPressed();
 			}
 		});
-		rdBtnTypeSubreddit.addChangeListener(new ChangeListener() {
+
+		/** Action Listeners. */
+		btnSignIn.addActionListener(new ActionListener() {
+
 			@Override
-			public void stateChanged(ChangeEvent e) {
-				logic.rdBtnTypeSubredditStateChanged();
-			}
-		});
-		rdBtnTypeUser.addChangeListener(new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				logic.rdBtnTypeUserStateChanged();
+			public void actionPerformed(ActionEvent evt) {
+				logic.btnSignInPressed();
 			}
 		});
 
@@ -674,5 +681,4 @@ public class View {
 	public void setTxtUsername(JFormattedTextField txtUsername) {
 		this.txtUsername = txtUsername;
 	}
-
 }
